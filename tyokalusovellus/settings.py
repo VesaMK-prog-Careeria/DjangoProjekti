1377d4d7a5c8c2e05704eec03bd3973a10a9ee40
Vesa Kalliomäki <vesa.kalliomaki@student.careeria.fi>
2024-09-23 19:56:13 +0300

    Debug

--- tyokalusovellus/settings.py
+++ tyokalusovellus/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-1(qo$w(r@-9v&ttg-g*3z#t9!*p*m%%4iyu6gxc%*2@k)b75hm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
